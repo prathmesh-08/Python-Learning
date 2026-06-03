1140bfe0517d171b63d2decaeb581c8cfd0659e4
Prathamesh Janardan Bedse <prathameshbedse06@gmail.com>
2026-06-03 21:00:16 +0530

    Add files via upload
    Added Day 2 Python practice files

---
+++ age prediator.py
@@ -0,0 +1,8 @@
+name=str(input("Enter your Name : "))
+age = int(input("Enter youe age : "))
+
+next_age= age + 1
+
+print( " Hello ",name)
+print( " Your Age is:",age)
+print( " You will Turn",next_age,"next year.")
---
+++ square and cube.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number: "))
+print("Square = ",num*num)
+print("Cube = ",num*num*num)
