131b97bc2eb113538170b7deda718d434c6be7ef
unknown <prathameshbedse06@gmail.com>
2026-08-17 23:14:48 +0530

    Added Day 4 Python exercises

---
+++ string function.py
@@ -0,0 +1,5 @@
+# name = "Prathamesh"   #length of string
+# print (len(name))
+
+# string = "Pratham"
+# print(string.endswith("am")) #prints ends with is true or false
--- string.py
+++ string.py
@@ -1,4 +1,20 @@
-name =('''Prathamesh studies in mit aoe second year''')
-print(name[0])
-for character in name:
-    print(character)
+# name =('''Prathamesh studies in mit aoe second year''')
+# print(name[0])
+# for character in name:
+#     print(character)
+
+#String Slicing
+# name = "pratham"
+
+# nameshort = name[0:3]
+# print(nameshort)  #start from index 0 till 3 (excluding 3 )
+# character1 = name[1]
+# print(character1)
+
+#Negative Slicing
+# namesort=name[-4:-1]
+# namesort=name[1:4]
+# print(namesort)
+word = "amazing"
+word[1:6:2]
+print(word)
